56b062a0f5c63cc754c3b9077af091fd8fecda1b
bearwithtea <132114816+bearwithtea@users.noreply.github.com>
2024-06-07 14:43:00 -0500

    chore: Update start script in package.json

---
+++ server.js
@@ -0,0 +1,34 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+
+// This will parse the body of incoming POST requests
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); // To parse JSON bodies
+
+// Read the votes from the file
+let votes = JSON.parse(fs.readFileSync('votes.json', 'utf8'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Default route to send the index.html file
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+}); 
+
+// Import your routes
+const submitVote = require('./api/submit-vote');
+const results = require('./api/results');
+const currentVotes = require('./api/current-votes');
+
+// Use your routes
+app.post('/submit-vote', submitVote);
+app.get('/results', results);
+app.get('/current-votes', currentVotes);
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
